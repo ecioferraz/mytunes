22d2e53cc79fe50844da07cde5e8a8dcc633fc6b
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-15 18:00:54 -0300

    refactor: save only email to localStorage

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -3,7 +3,9 @@ import { IUserInfo } from '../interfaces';
 const USER_KEY = 'mytunes_user';
 
 const readUser = () => JSON.parse(localStorage.getItem(USER_KEY) as string);
-const saveUser = (user: IUserInfo) =>
+const saveUser = (userInfo: IUserInfo) => {
+  const { password, ...user } = userInfo;
   localStorage.setItem(USER_KEY, JSON.stringify(user));
+};
 
 export { readUser, saveUser };
