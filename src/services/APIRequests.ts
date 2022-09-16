9e65ea325739e665eba4a1113ee3a5236760ad1f
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-16 15:15:54 -0300

    refactor: APIRequest

--- src/services/APIRequests.ts
+++ src/services/APIRequests.ts
@@ -6,17 +6,16 @@ const baseURL = 'https://itunes.apple.com/';
 const api = axios.create({ baseURL });
 
 export const getAlbums = async (artist: string) => {
-  const { data } = await api.get(
+  const { data: { results } } = await api.get(
     `search?entity=album&term=${artist}&attribute=allArtistTerm`
   );
 
-  return data.map(
+  return results.map(
     ({
       artistId,
       artistName,
       collectionId,
       collectionName,
-      collectionPrice,
       artworkUrl100,
       releaseDate,
       trackCount,
@@ -25,7 +24,6 @@ export const getAlbums = async (artist: string) => {
       artistName,
       collectionId,
       collectionName,
-      collectionPrice,
       artworkUrl100,
       releaseDate,
       trackCount,
