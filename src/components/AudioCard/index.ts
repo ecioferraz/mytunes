ab2e8d2db8bbfab53c0feb62cd3f01d53c55bb63
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-19 14:50:14 -0300

    feat: AudioCard

---
+++ src/components/AudioCard/index.ts
@@ -0,0 +1,6 @@
+import AudioCard from './AudioCard';
+import IAudioCard from './IAudioCard';
+
+export default AudioCard;
+
+export type { IAudioCard };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
+import AudioCard from './AudioCard';
 import Button from './Button';
 import ImageCard from './ImageCard';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
+  AudioCard,
   Button,
   ImageCard,
   TextCard,
