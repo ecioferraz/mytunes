b64313ee39fe79abf13b06b3dec6e1b5cec4ba58
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-19 15:01:02 -0300

    feat: HeaderLink

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import AudioCard from './AudioCard';
 import Button from './Button';
+import HeaderLink from './HeaderLink';
 import ImageCard from './ImageCard';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
@@ -7,6 +8,7 @@ import TextInput from './TextInput';
 export {
   AudioCard,
   Button,
+  HeaderLink,
   ImageCard,
   TextCard,
   TextInput,
