cd7c600d91e2652719a2a216b9a5f0e252d5ddd9
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-20 15:54:05 -0300

    feat: localStorage removeUser

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -3,9 +3,10 @@ import { IUserInfo } from '../interfaces';
 const USER_KEY = 'mytunes_user';
 
 const readUser = () => JSON.parse(localStorage.getItem(USER_KEY) as string);
+const removeUser = () => localStorage.removeItem(USER_KEY);
 const saveUser = (userInfo: IUserInfo) => {
   const { password, ...user } = userInfo;
   localStorage.setItem(USER_KEY, JSON.stringify(user));
 };
 
-export { readUser, saveUser };
+export { readUser, removeUser, saveUser };
