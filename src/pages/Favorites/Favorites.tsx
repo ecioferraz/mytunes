dcee92f95aaf0f85518dd7a6513335dd91bff2c6
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-21 15:15:12 -0300

    feat: Favorites

---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FavoritesCard } from '../../templates';
+
+export default function Favorites() {
+  return (
+    <main>
+      <FavoritesCard />
+    </main>
+  );
+}
